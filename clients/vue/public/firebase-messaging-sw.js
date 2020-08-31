f7eb7803207a8c8afe69ce4c8cc5c395d8aebedd
Klaus Plankensteiner <klaus.plankensteiner@gmx.at>
2020-08-31 20:33:27 +0200

    test

--- clients/vue/public/firebase-messaging-sw.js
+++ clients/vue/public/firebase-messaging-sw.js
@@ -20,4 +20,21 @@ firebase.initializeApp(firebaseConfig);
 
 // Retrieve an instance of Firebase Messaging so that it can handle background messages.
 const messaging = firebase.messaging();
+messaging.setBackgroundMessageHandler(function(payload) {
+  console.log(
+    "[firebase-messaging-sw.js] Received background message ",
+    payload
+  );
+  // Customize notification here
+  const notificationTitle = "Background Message Title";
+  const notificationOptions = {
+    body: "Background Message body.",
+    icon: "/firebase-logo.png",
+  };
+
+  return self.registration.showNotification(
+    notificationTitle,
+    notificationOptions
+  );
+});
 // [END initialize_firebase_in_sw]
