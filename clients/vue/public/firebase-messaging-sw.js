a4f9eaf27fa5fff9b309d7921dc892ef16ef8161
Klaus Plankensteiner <klaus.plankensteiner@gmx.at>
2020-08-31 20:57:51 +0200

    Revert "testing older firebase version"
    This reverts commit 0adea7861a809b439a6b8ffb2d8ca331a46d817b.

--- clients/vue/public/firebase-messaging-sw.js
+++ clients/vue/public/firebase-messaging-sw.js
@@ -9,9 +9,9 @@ self.addEventListener("notificationclick", function(event) {
 // Give the service worker access to Firebase Messaging.
 // Note that you can only use Firebase Messaging here, other Firebase libraries
 // are not available in the service worker.
-importScripts("https://www.gstatic.com/firebasejs/7.18.0/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/7.19.1/firebase-app.js");
 importScripts(
-  "https://www.gstatic.com/firebasejs/7.18.0/firebase-messaging.js"
+  "https://www.gstatic.com/firebasejs/7.19.1/firebase-messaging.js"
 );
 importScripts("./firebase.config.js");
 // Initialize the Firebase app in the service worker by passing in the messagingSenderId.
